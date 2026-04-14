10a04156cda56534c1bd9864a0898bbf2a5afa37
abrehman02 <115941928+abrehman02@users.noreply.github.com>
2026-04-15 04:42:07 +0530

    Refactor PrismaClient initialization for production

--- T3-Chat/src/lib/db.js
+++ T3-Chat/src/lib/db.js
@@ -4,9 +4,7 @@ const globalForPrisma = globalThis;
 
 export const db =
   globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["error"], // remove query logs for production
-  });
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = db;
